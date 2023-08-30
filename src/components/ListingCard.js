6fdcf8f2b7e6d1ef3e14b3f726663e7455d5d7c8
Terence Hughes <Terencehughes450@gmail.com>
2023-08-30 11:56:26 -0600

    in progress

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,29 @@
-import React from "react";
+//Add a useSate after the cards are rendered
+import React, { useState } from "react";
 
-function ListingCard() {
+//pass listing as a prop for the component to access the data. then fill in info with the correct elements to display
+function ListingCard({listing, deleteById}) {//once deleteById is set as a prop pass it to an onCLick in the button itself below
+ 
+  //create a state that sets the initial state to where nothing happens unless you click it
+  const [favorite, setFavortie]=useState(false)
+  
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={listing.image} alt={"description"} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
-        ) : (
-          <button className="emoji-button favorite">☆</button>
+      
+        {favorite ? (//add an onClick to the button and call setFavorite in an arrow function that returns the opposite of favorite once clicked
+          <button onClick={()=>setFavortie(!favorite)} className="emoji-button favorite active">★</button>
+        ) : (//follow the same steps for the button to return to its former state
+          <button onClick={()=>setFavortie(!favorite)} className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+      
+       <strong>{listing.description}</strong>
+        <span> · {listing.location}</span> 
+        <button onClick={()=>deleteById(listing.id)} className="emoji-button delete">🗑</button>
       </div>
     </li>
   );
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,45 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
-
+import React,{ useState, useEffect} from "react";
+import ListingCard from "./ListingCard";
+const API = 'http://localhost:6001/listings'
 function ListingsContainer() {
+  //create state using terminology that is relevent to the problem
+  const [listings, setListings]=useState([])//here we use an empty array to fetch the data
+  
+  
+  useEffect(()=>{
+    fetch(API)
+  .then(r=>r.json())
+  .then(data=>setListings(data))
+  },[])
+  //after the data is fetched create a .map to display the array items using a singular listing
+  const listingsCards = listings.map((listing) => {
+    //this return is creating multiple listing cards(imported from parent)
+     //this directly passes "listing" into the card
+    return <ListingCard key={listing.id}listing={listing} deleteById={deleteById}/>
+  })
+  //create a function at the highest level that allows you to delete specific id's
+  //this is the highest necessary component in this situation
+  function deleteById(id){
+    fetch(`http://localhost:6001/listings/${id}`, 
+    {method: 'DELETE'})
+    .then(r=>r.json)
+    .then(data =>{//using a filter here is the best way to delete from the listings array
+      const newListingArray = listings.filter((listing)=>{
+        //create an If statement that allows the filter function to compare id to lisitng ids and sort the odd one out by returning true or false
+        if(id===listing.id){
+          return false
+        }
+        return true
+      })
+     //here we are appending the new array to the page using setListings
+      setListings(newListingArray)
+    })
+  }//once the delete fetch has been created pass the data into the listing card, and then pass the function iteself as a prop into listingCard
+  
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingsCards}
       </ul>
     </main>
   );
